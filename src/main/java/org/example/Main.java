effb375783ef01b63683f55a61bd7be908663ea5
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-10-17 10:36:04 +0200

    Example code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,15 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int[] grades = new int[5];
+        grades[3] = 6;
+        grades[4] = 3;
+
+        for (int grade : grades) {
+            System.out.println("Eine Schulnote: "+grade);
+        }
+
+        System.out.println("Ende");
     }
 }
